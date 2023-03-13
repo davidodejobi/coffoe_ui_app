c3ca10ba86d4f3b13c97da67b6009d3929500ae9
davidaodejobi <talk2engineerdavid@gmail.com>
2023-03-13 10:28:36 +0100

    feat: completed home UI

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -238,9 +238,77 @@ class Home extends ConsumerWidget {
                 left: 20,
               ),
           20.heightBox,
-          // Container(
-          //   color: ,
-          // )
+          Container(
+            padding: const EdgeInsets.all(10),
+            height: 150.h,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  blurRadius: 2,
+                  offset: const Offset(0, -1),
+                ),
+              ],
+              gradient: LinearGradient(
+                colors: [
+                  Colors.black.withOpacity(0.3),
+                  Colors.black.withOpacity(0.5),
+                  Colors.black.withOpacity(1.0),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: const [
+                  0.0,
+                  0.5,
+                  1.0,
+                ],
+              ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    height: 200.h,
+                    width: 200.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      image: const DecorationImage(
+                        image: AssetImage(
+                          'assets/images/coffoe1.jpeg',
+                        ),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 6,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      10.heightBox,
+                      '5 Coffee Beans For You Must Try !'
+                          .addText(
+                            context,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 16,
+                          )
+                          .paddingOnly(
+                            left: 20,
+                          ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ).paddingSymmetric(
+            horizontal: 20,
+            vertical: 0,
+          ),
         ],
       ),
     );
